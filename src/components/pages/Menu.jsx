6b90503a0b3179a94ccb6c9c7d4aa9b5ba4ef08a
Yana Lachkova <lyana688@gmail.com>
2022-06-18 14:28:34 +0700

    Sorting, search and section name added

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -2,13 +2,20 @@
 import { useState } from "react"
 import ProductBlock from "../menu/ProductBlock"
 import Categories from "../menu/Categories"
+import Search from "../menu/Search"
+import Sort from "../menu/Sort"
 
 export default function Menu() {
     const [activeIndex, setActiveIndex] = useState(0)
 
     return (
-        <div >
+        <>
             <Categories activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+            <div className="section_header">
+                <p>{activeIndex}</p>
+                <Search />
+                <Sort />
+            </div>
             <div className="product_wrapper">
                 <div className="products_list">
                     {[...new Array(5)].map(() =>
@@ -16,6 +23,6 @@ export default function Menu() {
                     )}
                 </div>
             </div>
-        </div>
+        </> 
     )
 }
