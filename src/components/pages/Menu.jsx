ad75fd4e09a2d09088bfa5be9e8cfb94130e3bbe
Yana Lachkova <lyana688@gmail.com>
2022-06-18 13:29:20 +0700

    Category selection section completed ____________________ By clicking on the section, the activeIndex state is set, the name color changes

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,9 +1,37 @@
 import ProductBlock from "../ProductBlock"
-export default function Menu(){
+import pizza from "../../assets/img/food_units/pizza.svg"
+import drinks from "../../assets/img/food_units/drinks.svg"
+import des from "../../assets/img/food_units/des.svg"
+import vegan from "../../assets/img/food_units/vegan.svg"
+import { useState } from "react"
+
+export default function Menu() {
+    const [activeIndex, setActiveIndex] = useState(0)
+    console.log(activeIndex)
     return (
         <div>
-            jjj
-            <ProductBlock />
+            <div className="menu_units">
+                <div className={activeIndex == 0 ? "item item--active" : "item"}
+                    onClick={() => { setActiveIndex(0) }}>
+                    <img src={pizza} alt="pizza" />
+                    <p>Пицца</p>
+                </div>
+                <div className={activeIndex == 1 ? "item item--active" : "item"}
+                    onClick={() => { setActiveIndex(1) }}>
+                    <img src={drinks} alt="drinks" />
+                    <p>Напитки</p>
+                </div>
+                <div className={activeIndex == 2 ? "item item--active" : "item"}
+                    onClick={() => { setActiveIndex(2) }}>
+                    <img src={des} alt="desserts" />
+                    <p>Десерты</p>
+                </div>
+                <div className={activeIndex == 3 ? "item item--active" : "item"}
+                    onClick={() => { setActiveIndex(3) }}>
+                    <img src={vegan} alt="vegan" />
+                    <p>Веган</p>
+                </div>
+            </div>
         </div>
     )
 }
